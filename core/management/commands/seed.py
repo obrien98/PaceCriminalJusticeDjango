a752d8c3de2ff9d117038d60576035545d5d5176
obrien98 <justin.obrien7698@gmail.com>
2026-04-27 14:16:27 -0400

    update seed.py to contain more events

--- core/management/commands/seed.py
+++ core/management/commands/seed.py
@@ -123,6 +123,14 @@ class Command(BaseCommand):
             location="Miller Hall 22, Active Learning Classroom",
             description="Hear from a criminal investigator with the White Plains Department of Public Safety about the investigative process.",
         )
+        Event.objects.create(
+            name="Trivia Night",
+            date=date(2026, 4, 28),
+            time=time(12, 10),
+            location="KESSEL MULTIPURPOSE ROOM, ROOM 206",
+            description="Join the Criminal Justice Society for a night of trivia!\n We will have Wingstop, Prizes, Giveaways and more!.",
+        )
+        
 
         # -------- GALLERY IMAGES --------
         image_urls = [
